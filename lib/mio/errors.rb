b2dea1ffb74e03bfb56e0355d7d1349a6aa8ace4
jspc <james@zero-internet.org.uk>
2016-03-24 16:30:21 +0000

    Provide Mio::Model error classes

--- lib/mio/errors.rb
+++ lib/mio/errors.rb
@@ -2,4 +2,11 @@ class Mio
   class Client
     class LoadOfBollocks < ArgumentError ; end
   end
+
+  class Models
+    class NoSuchField < ArgumentError ; end
+    class MissingField < ArgumentError ; end
+    class DataTypeError < ArgumentError ; end
+    class DataValueError < ArgumentError ; end
+  end
 end
