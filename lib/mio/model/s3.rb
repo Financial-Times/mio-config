3d1ed2e997d6251de91b8da348c0b22c5595b8fa
jspc <james@zero-internet.org.uk>
2016-03-29 10:44:33 +0100

    Create full S3 object

--- lib/mio/model/s3.rb
+++ lib/mio/model/s3.rb
@@ -1,22 +1,51 @@
 class Mio
   class Model
-    class Resource < Model
+    class S3 < Model
       resource :resources
 
       field :name, String, /^(?!\s*$).+/
       field :visibility, Array
-      field :type, Symbol
+      field :key, String
+      field :secret_key, String
+      field :bucket, String
+
+      field :enable, Symbol
+      field :start, Symbol
 
       def create
         # Override create from Mio::Models::Base
-        type = {
-          vfs: 'tv.nativ.mio.enterprise.resources.impl.capacity.storage.vfs.VFSStorageResource'
-        }[@args[:type]]
+        plugin = 'tv.nativ.mio.enterprise.resources.impl.capacity.storage.vfs.VFSStorageResource'
 
-        @client.create 'resources',
-                       {name: @args[:name],
-                        pluginClass: type,
-                        visibilityIds: @args[:visibility]}
+        object = @client.create 'resources',
+                                {name: @args[:name],
+                                 pluginClass: plugin,
+                                 visibilityIds: @args[:visibility]}
+        configure_payload = {
+          'vfs-location' => {
+            protocol: "S3",
+            path: "/",
+            key: @args[:key],
+            secret: @args[:secret_key],
+            bucket: @args[:bucket]
+          }
+        }
+
+        @client.configure 'resources',
+                          object.id,
+                          configure_payload
+
+        if @args[:enable] == :true
+          @client.action 'resources',
+                         object.id,
+                         {action: 'enable'}
+        end
+
+        if @args[:start] == :true
+          @client.action 'resources',
+                         object.id,
+                         {action: 'start'} if @args[:start]
+        end
+        object
       end
 
     end
