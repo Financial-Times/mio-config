27f96fbca0370ef5d28f742a38c17d70dffef311
jspc <james@zero-internet.org.uk>
2016-03-30 09:07:58 +0100

    Ignore migrations

--- migrations/02_jems_ferking_wank.rb
+++
@@ -1,9 +0,0 @@
-migrate 'create boobies S3 VFS thing' do
-  s3 do |s|
-    s.name = 'jems_ingester'
-    s.visibility = [4]
-    s.key = 'some-bollocks'
-    s.secret = 'some-secret-bollocks'
-    s.bucket = 'jems_shite_bucket'
-  end
-end
