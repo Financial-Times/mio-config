06724df63985f11f99a46b94d8cb5a165f9a57ac
jspc <james@zero-internet.org.uk>
2016-03-30 16:25:13 +0100

    Delegate search to Mio::Search

--- lib/mio/model/hotfolder.rb
+++ lib/mio/model/hotfolder.rb
@@ -23,42 +23,30 @@ class Mio
 
       def config_array
         # Get the s3 resource id
-        storages = @client.find_all('resources')
-        storage = storages.resources.find do |s|
-          s.name == @args.storage_resource_name
-        end
-        unless storage
-          raise Mio::Model::NoSuchResource, "Could not find #{@args.storage_resource_name}"
-        end
-        storage_id = storage.id
+        storage = @search.find_resources_by_name(@args.storage_resource_name).first
+        raise Mio::Model::NoSuchResource, "Could not find #{@args.storage_resource_name}" unless storage
 
         # Get the workflow name
-        workflows = @client.find_all('workflows')
-        workflow = workflows.workflows.find do |s|
-          s.name == @args.workflow_name
-        end
-        unless workflow
-          raise Mio::Model::NoSuchResource, "Could not find #{@args.workflow_name}"
-        end
-        workflow_id = workflow.id
+        workflow = @search.find_workflows_by_name(@args.workflow_name).first
+        raise Mio::Model::NoSuchResource, "Could not find #{@args.workflow_name}" unless workflow
 
-        # User ID
-        owner_id = @client.find_all('users').users.find{|u| u.displayName == @args.owner}.id
+        owner = @search.find_users_by_displayName(@args.owner).first
+        raise Mio::Model::NoSuchResource, "Could not find #{@args.owner}" unless owner
 
         {
           'housekeeping-period' => 5139767606400000000,
           'inactivity-timeout' => 30000000,
           'storage-resources' => {
             'Storage Resource' => [
-              {'id' =>  storage_id }
+              {'id' =>  storage.id }
             ]
           },
           'use-md5' => false,
           'workflow-definition' => {
-            'id' =>  workflow_id
+            'id' =>  workflow.id
           },
           'owner' => {
-            'id' =>  owner_id
+            'id' =>  owner.id
           }
         }
       end
