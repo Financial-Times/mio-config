c64f48665c4a264833302c805dedaea29415a4d4
jspc <james@zero-internet.org.uk>
2016-03-30 09:05:27 +0100

    Set hotfolder owner

--- lib/mio/model/hotfolder.rb
+++ lib/mio/model/hotfolder.rb
@@ -5,6 +5,7 @@ class Mio
 
       field :storage_resource_name, String
       field :workflow_name, String
+      field :owner, String
 
       field :name, String, /^(?!\s*$).+/
       field :visibility, Array
@@ -13,10 +14,9 @@ class Mio
       field :start, Symbol
 
       def create
-
         # Get the s3 resource id
         storages = @client.find_all('resources')
-        storage = storages.find do |s| 
+        storage = storages.resources.find do |s|
           s.name == @args[:storage_resource_name]
         end
         unless storage
@@ -26,7 +26,7 @@ class Mio
 
         # Get the workflow name
         workflows = @client.find_all('workflows')
-        workflow = workflows.find do |s| 
+        workflow = workflows.workflows.find do |s|
           s.name == @args[:workflow_name]
         end
         unless workflow
@@ -34,6 +34,8 @@ class Mio
         end
         workflow_id = workflow.id
 
+        # User ID
+        owner_id = @client.find_all('users').users.find{|u| u.displayName == @args[:owner]}.id
 
         # Override create from Mio::Models::Base
         plugin = 'tv.nativ.mio.enterprise.resources.impl.capacity.folder.hotfolder.MioHotFolderResource'
@@ -43,28 +45,20 @@ class Mio
                                  pluginClass: plugin,
                                  visibilityIds: @args[:visibility]}
         configure_payload = {
-              'housekeeping-period' => 5139767606400000000,
-              'inactivity-timeout' => 30000000,
-              'storage-resources' => {
-                                       'Storage Resource' => [
-            {
-                    'id' =>  storage_id
-                            
-        }
-              
-        ]
-            
-        },
-              'use-md5' => false,
-              'workflow-definition' => {
-                 'id' =>  workflow_id
-            
-        },
+          'housekeeping-period' => 5139767606400000000,
+          'inactivity-timeout' => 30000000,
+          'storage-resources' => {
+            'Storage Resource' => [
+              {'id' =>  storage_id }
+            ]
+          },
+          'use-md5' => false,
+          'workflow-definition' => {
+            'id' =>  workflow_id
+          },
           'owner' => {
-                 'id' =>  @args[:visibility].first
-            
-        }
-
+            'id' =>  owner_id
+          }
         }
 
         @client.configure 'resources',
