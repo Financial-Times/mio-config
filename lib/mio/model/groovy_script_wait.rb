f661658a6d0fb3660fe7f8fe61e373d232cb7aeb
Jem Rayfield <jem.rayfield@ft.com>
2016-04-07 10:42:31 +0100

    Clean up whitespace

--- lib/mio/model/groovy_script_wait.rb
+++ lib/mio/model/groovy_script_wait.rb
@@ -7,12 +7,12 @@ class Mio
       field :displayName, String, 'Script Display name'
       field :key, String, 'AWS API Key with access to S3'
       field :secret, String, 'AWS secret'
-      field :visibility, Array, 'Ids of the accounts which may see the import action' 
+      field :visibility, Array, 'Ids of the accounts which may see the import action'
       field :script, String, 'The groovy script (inline) ', '"File.read(/path/to/script.groovy)"'
-      field :jars, Array, 'JARs to load on remote, empty for none', [] 
+      field :jars, Array, 'JARs to load on remote, empty for none', []
       field :imports, Array, 'Imports to reference within groovy script, empty for none', []
-      field :timeout, Integer, 'Time to give up waiting', 0
-      field :pollingTime, Integer, 'Time to wait between script invocation', 100000
+      field :timeout, Fixnum, 'Time to give up waiting', 0
+      field :polling_time, Fixnum, 'Time to wait between script invocation', 100000
 
       field :enable, Symbol, ':true or :false', :true
       field :start, Symbol, ':true or :false', :true
@@ -28,11 +28,12 @@ class Mio
       end
 
       def config_hash
-        {"script_type": 
-         {"script": "def execute() {}"},
-         "imports": {
-          'jar-url': @args.jars.map{|jar| {value: jar, isExpression: false}},
-          import: @args.imports.map{|import| {value: import, isExpression: false}}
+        {"script_type": {
+         "script": @args.script
+        },
+        "imports": {
+          'jar-url': @args.jars.map{|j| {value: j, isExpression: false}},
+          import: @args.imports.map{|i| {value: i, isExpression: false}}
         },
         "timeout": @args.timeout,
         "polling-time-period": @args.pollingTime
