84485e485e4098eeb44add929690b2a6846243d9
jspc <james@zero-internet.org.uk>
2016-04-14 12:44:34 +0100

    Handle different action types

--- lib/mio/model/workflow/node.rb
+++ lib/mio/model/workflow/node.rb
@@ -5,13 +5,31 @@ class Mio
         set_resource :workflows
 
         field :name, String, 'Nodal Name'
-        field :path, String, 'Path of node', '/Start'
+        field :action, String, 'Name of the action, empty for none', 'Load S3'
+        field :type, String, 'Action Type', 'ACTION'
 
         nested true
 
         def create_hash
-          {name: @args.name,
-           path: @args.path}
+          h = {name: @args.name,
+               path: "/#{@args.path}",
+               type: @args.type.upcase,
+              }
+          if @args.type.upcase == 'ACTION'
+            action = @search.find_actions_by_name(@args.action).first
+            h[:action] = normalize_action action
+
+          end
+          h
+        end
+
+        def normalize_action a
+          {
+            id: a['id'],
+            name: a['name'],
+            pluginClass: a['pluginClass']
+          }
+
         end
 
       end
